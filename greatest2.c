f095ad22977db0d25386f9a474c84fbc180c70e5
Yuvraj Chaudhary <yuvrajsinghchaudhary7@gmail.com>
2025-11-30 00:11:27 +0530

    Greatest of 2 numbers

---
+++ greatest2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10,b = 5;
+
+    if(a > b){
+        printf("%d is greater than %d",a,b);
+    }else{
+        printf("%d is greater than %d",b,a);
+    }
+    return 0;
+}
